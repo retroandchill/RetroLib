8c3e33924d7a1dfcabe288acecca9677c858e40f
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-28 14:34:57 -0500

    Updated some syntax and added documentation

--- RetroLib/include/RetroLib/Concepts/OpaqueStorage.h
+++ RetroLib/include/RetroLib/Concepts/OpaqueStorage.h
@@ -15,8 +15,20 @@
 #endif
 
 namespace Retro {
+    /**
+     * Global constant used to determine the size of the small buffer for opaque storage types.
+     */
     RETROLIB_EXPORT constexpr size_t SMALL_STORAGE_SIZE = sizeof(void *) * 7;
 
+    /**
+     * @brief Concept that verifies if a type can fit into small storage.
+     *
+     * The `CanFitSmallStorage` concept checks whether the size of a given type `T` is less than
+     * or equal to the predefined constant `SMALL_STORAGE_SIZE`. This allows for constraints
+     * on template parameters to ensure that only types fitting within a certain size are considered.
+     *
+     * @tparam T The type to be checked.
+     */
     RETROLIB_EXPORT template<typename T>
     concept CanFitSmallStorage = sizeof(T) <= SMALL_STORAGE_SIZE;
 
