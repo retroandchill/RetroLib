5c2d5807b828eff4accc914b1e835911a2a449d8
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-11-29 12:15:02 -0500

    Added additional tests for handling coverage

--- Tests/Utils/PolymorphicTest.cpp
+++ Tests/Utils/PolymorphicTest.cpp
@@ -67,6 +67,7 @@ private:
 };
 
 TEST_CASE("Polymorphic types can be instantiated and copied", "[utils]") {
+    // We want to test that we can assign different polymorphic values into each other
     Retro::Polymorphic<Base> polymorphic1 = Derived1(42);
     CHECK(polymorphic1->getValue() == 42);
 
@@ -77,6 +78,7 @@ TEST_CASE("Polymorphic types can be instantiated and copied", "[utils]") {
     polymorphic1 = polymorphic2;
     CHECK(polymorphic1->getValue() == 120);
 
+    // Here we want to check that when we reassignment the destructor for a different type gets invoked
     auto value = std::make_shared<int>(4);
     std::weak_ptr<int> weakValue = value;
     polymorphic1 = Derived3(std::move(value));
@@ -85,4 +87,27 @@ TEST_CASE("Polymorphic types can be instantiated and copied", "[utils]") {
     polymorphic1 = Retro::Polymorphic<Base>();
     CHECK(polymorphic1->getValue() == 0);
     CHECK(weakValue.expired());
+
+    // Here we want to verify that the polymorphic values can copy into like types when using small storage
+    polymorphic1 = Derived1(12);
+    polymorphic2 = Derived1(64);
+    CHECK(polymorphic1->getValue() == 12);
+    CHECK(polymorphic2->getValue() == 64);
+
+    polymorphic1 = polymorphic2;
+    CHECK(polymorphic1->getValue() == 64);
+    polymorphic1 = Retro::Polymorphic<Base>(std::in_place_type<Derived1>, 100);
+    CHECK(polymorphic1->getValue() == 100);
+
+    // Here we're going to do the same thing with large storage
+    polymorphic1.emplace<Derived2>(std::array{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+    polymorphic2.emplace<Derived2>(std::array{2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30});
+    CHECK(polymorphic1->getValue() == 120);
+    CHECK(polymorphic2->getValue() == 240);
+
+    polymorphic1 = polymorphic2;
+    CHECK(polymorphic1->getValue() == 240);
+    polymorphic1 = Retro::Polymorphic<Base>(std::in_place_type<Derived2>,
+            std::array{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+    CHECK(polymorphic1->getValue() == 120);
 }
