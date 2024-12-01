77860d186efd0194ee0badc2030cee254fb21aac
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-12-01 12:02:15 -0500

    Added documentation to the new files

--- RetroLib/include/RetroLib/Utils/ForwardLike.h
+++ RetroLib/include/RetroLib/Utils/ForwardLike.h
@@ -32,10 +32,10 @@ namespace retro {
      * @note The behavior of the function mimics the effects of std::forward but extends
      *       it by adding the ability to replicate cv-qualifiers from a template type `T`.
      */
-    RETROLIB_EXPORT template<class T, class U>
-    constexpr auto&& forward_like(U&& x) noexcept {
+    RETROLIB_EXPORT template <class T, class U>
+    constexpr auto &&forward_like(U &&x) noexcept {
         constexpr bool is_adding_const = std::is_const_v<std::remove_reference_t<T>>;
-        if constexpr (std::is_lvalue_reference_v<T&&>) {
+        if constexpr (std::is_lvalue_reference_v<T &&>) {
             if constexpr (is_adding_const) {
                 return std::as_const(x);
             } else {
@@ -49,4 +49,4 @@ namespace retro {
             }
         }
     }
-} // namespace Retro
+} // namespace retro
--- Tests/Ranges/Algorithm/RangesToTest.cpp
+++ Tests/Ranges/Algorithm/RangesToTest.cpp
@@ -5,9 +5,9 @@
  * @author Retro & Chill
  * https://github.com/retroandchill
  */
+#include <catch2/catch_test_macros.hpp>
 #include <map>
 #include <set>
-#include <catch2/catch_test_macros.hpp>
 
 #if RETROLIB_WITH_MODULES
 import std;
@@ -22,28 +22,24 @@ import RetroLib;
 TEST_CASE("Ranges can be converted into a collection type") {
     static constexpr std::array values = {1, 2, 3, 4, 5};
     SECTION("Convert to a like range type") {
-        auto vectored = values |
-            retro::ranges::to<std::vector>();
+        auto vectored = values | retro::ranges::to<std::vector>();
         CHECK(vectored == std::vector({1, 2, 3, 4, 5}));
     }
 
     SECTION("Can convert between unlike, but compatible, range types") {
-        auto vectored = values |
-            retro::ranges::to<std::vector<double>>();
+        auto vectored = values | retro::ranges::to<std::vector<double>>();
         CHECK(vectored == std::vector({1.0, 2.0, 3.0, 4.0, 5.0}));
     }
 
     SECTION("Can convert a vector into a set") {
         static constexpr std::array duplicated = {1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
-        auto as_set = duplicated |
-            retro::ranges::to<std::set>();
+        auto as_set = duplicated | retro::ranges::to<std::set>();
         CHECK(as_set == std::set({1, 2, 3}));
     }
 
     SECTION("Can convert a range into a map using pairs") {
         static constexpr std::array pairs = {std::make_pair(1, 2), std::make_pair(3, 4)};
-        auto as_map = pairs |
-            retro::ranges::to<std::map<int, int>>();
+        auto as_map = pairs | retro::ranges::to<std::map<int, int>>();
         CHECK(as_map == std::map<int, int>({{1, 2}, {3, 4}}));
     }
 }
