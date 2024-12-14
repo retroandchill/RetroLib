27b12f5d1e978fa8c2d64f7bf34e3c2b27542b7c
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-12-14 10:51:48 -0500

    Added documentation

--- RetroLib/include/RetroLib/Concepts/Tuples.h
+++ RetroLib/include/RetroLib/Concepts/Tuples.h
@@ -19,6 +19,13 @@
 #endif
 
 namespace retro {
+    /**
+     * The type of forwarded tuple element. Normally it's the same as using `forward_like`, except when the tuple
+     * element is an l-value reference, where the reference is retained.
+     *
+     * @tparam T The tuple index
+     * @tparam I The index to look up
+     */
     RETROLIB_EXPORT template <typename T, size_t I>
     using ForwardedTupleType = std::conditional_t<std::is_lvalue_reference_v<std::tuple_element_t<I, std::decay_t<T>>>,
         std::tuple_element_t<I, std::decay_t<T>>,
@@ -41,7 +48,7 @@ namespace retro {
      * Evaluates at compile-time if all specified indices in a given index sequence
      * have an associated tuple element defined for the type `T`.
      *
-     * @param seq A compile-time index sequence represented by std::index_sequence<I...>.
+     * @param std::index_sequence<I...> A compile-time index sequence.
      *            It contains a sequence of indices that will be checked to ensure each
      *            corresponds to a valid tuple element in `T`.
      *
@@ -64,25 +71,71 @@ namespace retro {
         { std::tuple_size_v<T> } -> std::convertible_to<size_t>;
     } && all_has_tuple_element<T>(std::make_index_sequence<std::tuple_size_v<T>>{});
 
+    /**
+     * Checks if the given args can be applied to the given functor type.
+     *
+     * @tparam F The functional type
+     * @tparam T The tuple type
+     * @param I The indicies that are to be applied as arguments
+     */
     template <typename F, typename T, size_t... I>
     concept CanApplyArgs = HasFunctionCallOperator<std::decay_t<F>> && TupleLike<std::decay_t<T>> && std::invocable<F, ForwardedTupleType<T, I>...>;
 
+    /**
+     * Determines if the template arguments can be applied with an index sequence.
+     *
+     * @param std::index_sequence<I...> An index sequence of integers used to check
+     *                                  applicability of template arguments.
+     * @return bool Returns true if the template arguments can be successfully applied with
+     *              the given index sequence, otherwise false.
+     */
     template <typename F, typename T, size_t... I>
     consteval bool can_apply_index_sequence(std::index_sequence<I...>) {
         return CanApplyArgs<F, T, I...>;
     }
 
+    /**
+     * Checks if the tuple can be applied to the given functional callback using `std::apply`.
+     *
+     * @tparam F The functional type
+     * @tparam T The tuple type
+     */
     RETROLIB_EXPORT template <typename F, typename T>
     concept CanApply = HasFunctionCallOperator<std::decay_t<F>> && TupleLike<std::decay_t<T>> && can_apply_index_sequence<F, T>(std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>{});
 
+    /**
+     * Checks if the given args when applied to the given functor type is noexcept.
+     *
+     * @tparam F The functional type
+     * @tparam T The tuple type
+     * @param I The indicies that are to be applied as arguments
+     */
     template <typename F, typename T, size_t... I>
     concept NoThrowApplicableArgs = HasFunctionCallOperator<std::decay_t<F>> && TupleLike<std::decay_t<T>> && std::is_nothrow_invocable_v<F, ForwardedTupleType<T, I>...>;
 
+    /**
+     * @brief Determines whether a given set of arguments can be applied to a callable
+     * object without throwing exceptions at compile-time.
+     *
+     * @tparam F The callable object to check.
+     * @tparam T The type arguments to apply to the callable object.
+     * @param[in] std::index_sequence<I...> The sequence of indices representing the
+     * positional argument indices used for the check.
+     * @retval true If the callable object can be applied with the given arguments
+     *         without throwing exceptions.
+     * @retval false If the callable object cannot be applied without throwing
+     *         exceptions.
+     */
     template <typename F, typename T, size_t... I>
     consteval bool is_nothrow_applicable(std::index_sequence<I...>) {
         return NoThrowApplicableArgs<F, T, I...>;
     }
-
+    /**
+     * Checks if the given args when applied to the given functor type is noexcept.
+     *
+     * @tparam F The functional type
+     * @tparam T The tuple type
+     */
     RETROLIB_EXPORT template <typename F, typename T>
     concept NoThrowApplicable = HasFunctionCallOperator<std::decay_t<F>> && TupleLike<std::decay_t<T>> && is_nothrow_applicable<F, T>(std::make_index_sequence<std::tuple_size_v<std::decay_t<T>>>{});
 }
--- RetroLib/include/RetroLib/Functional/CreateBinding.h
+++ RetroLib/include/RetroLib/Functional/CreateBinding.h
@@ -30,42 +30,115 @@ namespace retro {
      */
     RETROLIB_EXPORT constexpr ThisType this_type;
 
+    /**
+     * @brief A utility class that wraps a callable object, enabling invocation and optional state management.
+     *
+     * The WrappedFunctor class serves as a mechanism to encapsulate a callable object, such as a lambda,
+     * function pointer, or function object, allowing the object to be managed and invoked with ease. This
+     * abstraction is particularly useful for scenarios requiring dynamic handling of callables or delayed
+     * execution.
+     */
     template <HasFunctionCallOperator F>
     struct WrappedFunctor {
+        /**
+         * @brief Constructs a WrappedFunctor by forwarding the given functor.
+         *
+         * @param functor The functor object to be wrapped, which will be forwarded.
+         */
         template <typename T>
             requires std::constructible_from<F, T> && (!std::same_as<std::decay_t<T>, WrappedFunctor>)
         constexpr explicit WrappedFunctor(T&& functor) : functor(std::forward<T>(functor)) {}
 
+        /**
+         * @brief Invokes the stored functor with the provided arguments.
+         *
+         * The operator performs the invocation of the stored callable object
+         * using the provided arguments, ensuring that the invocation conforms
+         * to the noexcept condition based on the properties of the functor and arguments.
+         *
+         * @param args The arguments to be forwarded to the stored functor.
+         * @return The result of invoking the stored functor with the forwarded arguments.
+         *         The exact return type depends on the functor and the arguments.
+         */
         template <typename... A>
             requires std::invocable<F&, A...>
         constexpr decltype(auto) operator()(A&&... args) & noexcept(std::is_nothrow_invocable_v<F&, A...>) {
             return std::invoke(functor, std::forward<A>(args)...);
         }
 
+        /**
+         * @brief Invokes the stored functor with the provided arguments.
+         *
+         * The operator performs the invocation of the stored callable object
+         * using the provided arguments, ensuring that the invocation conforms
+         * to the noexcept condition based on the properties of the functor and arguments.
+         *
+         * @param args The arguments to be forwarded to the stored functor.
+         * @return The result of invoking the stored functor with the forwarded arguments.
+         *         The exact return type depends on the functor and the arguments.
+         */
         template <typename... A>
             requires std::invocable<const F&, A...>
         constexpr decltype(auto) operator()(A&&... args) const& noexcept(std::is_nothrow_invocable_v<const F&, A...>) {
             return std::invoke(functor, std::forward<A>(args)...);
         }
 
+        /**
+         * @brief Invokes the stored functor with the provided arguments.
+         *
+         * The operator performs the invocation of the stored callable object
+         * using the provided arguments, ensuring that the invocation conforms
+         * to the noexcept condition based on the properties of the functor and arguments.
+         *
+         * @param args The arguments to be forwarded to the stored functor.
+         * @return The result of invoking the stored functor with the forwarded arguments.
+         *         The exact return type depends on the functor and the arguments.
+         */
         template <typename... A>
             requires std::invocable<F, A...>
         constexpr decltype(auto) operator()(A&&... args) && noexcept(std::is_nothrow_invocable_v<F, A...>) {
             return std::invoke(std::move(functor), std::forward<A>(args)...);
         }
 
+        /**
+         * @brief Invokes the functor with the given arguments using `std::apply`.
+         *
+         * This function invokes the stored functor with the provided arguments, forwarding them appropriately.
+         * The operation adheres to the noexcept specification determined by the `NoThrowApplicable` trait.
+         *
+         * @param args The arguments to be forwarded to the functor.
+         * @return The result of invoking the functor with the forwarded arguments.
+         */
         template <typename U>
             requires CanApply<F&, U>
         constexpr decltype(auto) operator()(U&& args) & noexcept(NoThrowApplicable<F&, U>) {
             return std::apply(functor, std::forward<U>(args));
         }
 
+        /**
+         * @brief Invokes the functor with the given arguments using `std::apply`.
+         *
+         * This function invokes the stored functor with the provided arguments, forwarding them appropriately.
+         * The operation adheres to the noexcept specification determined by the `NoThrowApplicable` trait.
+         *
+         * @param args The arguments to be forwarded to the functor.
+         * @return The result of invoking the functor with the forwarded arguments.
+         */
         template <typename U>
             requires CanApply<const F&, U>
         constexpr decltype(auto) operator()(U&& args) const & noexcept(NoThrowApplicable<const F&, U>) {
             return std::apply(functor, std::forward<U>(args));
         }
 
+        /**
+         * @brief Invokes the functor with the given arguments using `std::apply`.
+         *
+         * This function invokes the stored functor with the provided arguments, forwarding them appropriately.
+         * The operation adheres to the noexcept specification determined by the `NoThrowApplicable` trait.
+         *
+         * @param args The arguments to be forwarded to the functor.
+         * @return The result of invoking the functor with the forwarded arguments.
+         */
         template <typename U>
             requires CanApply<F, U>
         constexpr decltype(auto) operator()(U&& args) && noexcept(NoThrowApplicable<F, U>) {
@@ -76,6 +149,12 @@ namespace retro {
         F functor;
     };
 
+    /**
+     * Creates a WrappedFunctor object by deducing and decaying the type of the input functor.
+     *
+     * @tparam F The input functor to be wrapped.
+     * @return A WrappedFunctor object with the decayed type of the input functor.
+     */
     template <HasFunctionCallOperator F>
     WrappedFunctor(F) -> WrappedFunctor<std::decay_t<F>>;
 
