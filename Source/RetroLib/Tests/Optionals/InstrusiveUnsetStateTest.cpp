77b15540d38a1523c55f0e5c4eee0b43563ffc3d
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-01-01 11:49:26 -0500

    Pulled in the rest of the plugin

---
+++ Source/RetroLib/Tests/Optionals/InstrusiveUnsetStateTest.cpp
@@ -0,0 +1,15 @@
+﻿#if WITH_TESTS
+#include "Tests/TestHarnessAdapter.h"
+#include "RetroLib/Optionals/Compatiblity/UnrealOptional.h"
+
+TEST_CASE_NAMED(FOptionalUnsetTest, "RetroLib::Optionals::Unset", "[RetroLib][Optionals]") {
+	SECTION("Can use the unset state of an Unreal type with a Retro::Optional") {
+		static_assert(Retro::HasAlternateIntrusiveUnsetState<TArray<int32>>);
+		static_assert(sizeof(TArray<int32>) == sizeof(Retro::Optional<TArray<int32>>));
+		Retro::Optional OptionalValue = TArray<int32>();
+		CHECK(OptionalValue.HasValue());
+		OptionalValue.Reset();
+		CHECK_FALSE(OptionalValue.HasValue());
+	}
+}
+#endif
